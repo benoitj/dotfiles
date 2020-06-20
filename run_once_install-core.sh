ac92524aa6ba27f740c27da6ea2c219156d21491
Benoit Joly <benoit@benoitj.ca>
2020-06-20 09:44:48 -0400

    installing more tools/dotfiles required to start x

--- run_once_install-core.sh
+++ run_once_install-core.sh
@@ -11,10 +11,12 @@ test -d ~/src/dmenu || git clone https://github.com/benoitj/dmenu ~/src/dmenu
 which dmenu || (cd ~/src/dmenu; make; sudo make install)
 test -d ~/src/st || git clone https://github.com/benoitj/st ~/src/st
 which st || (cd ~/src/st; make; sudo make install)
-test -d ~/src/dmenu || git clone https://github.com/benoitj/dwm ~/src/dwm
+test -d ~/src/dwm || git clone https://github.com/benoitj/dwm ~/src/dwm
 which dwm || (cd ~/src/dwm; make; sudo make install)
+test -d ~/src/dwmstatus || git clone https://github.com/benoitj/dwmstatus ~/src/dwmstatus
+which dwmstatus || (cd ~/src/dwmstatus; make; sudo make install)
 
-$PACMAN dunst sxhkd
+$PACMAN dunst sxhkd compton mate-polkit
 $PACMAN vim
 $PACMAN pulsemixer pamixer mpc
 $PACMAN surfraw
