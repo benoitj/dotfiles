7823832f31e7aef0d04fbd11f858cf04305fc1c5
Benoit Joly <benoit@benoitj.ca>
2020-07-01 17:32:58 -0400

    convert to doom emacs config

--- dot_emacs.d/config/app-logging.el
+++
@@ -1,5 +0,0 @@
-;;; -*- lexical-binding: t -*-
-;;; DONE
-
-(use-package command-log-mode
-  :commands (command-log-mode global-command-log-mode))
--- dot_emacs.d/config/lang-plantuml.el
+++
@@ -1,7 +0,0 @@
-;;; -*- lexical-binding: t -*-
-
-(use-package plantuml-mode
-  :config
-  (setq plantuml-default-exec-mode 'jar)
-  (setq plantuml-output-type "svg")
-  (setq plantuml-java-args '("-jar")))
