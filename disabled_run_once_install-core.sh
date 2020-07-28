04feb637df18371ef380392138cf95417484f116
Benoit Joly <benoit@benoitj.ca>
2020-07-28 19:09:34 -0400

    new updates

---
+++ disabled_run_once_install-core.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PACMAN="sudo pacman -S --noconfirm --needed"
+YAY="yay -S --needed"
+
+
+
+test -d ~/src || mkdir -p ~/src
+
+test -d ~/src/dmenu || git clone https://github.com/benoitj/dmenu ~/src/dmenu
+which dmenu || (cd ~/src/dmenu; make; sudo make install)
+test -d ~/src/st || git clone https://github.com/benoitj/st ~/src/st
+which st || (cd ~/src/st; make; sudo make install)
+test -d ~/src/dwm || git clone https://github.com/benoitj/dwm ~/src/dwm
+which dwm || (cd ~/src/dwm; make; sudo make install)
+test -d ~/src/dwmstatus || git clone https://github.com/benoitj/dwmstatus ~/src/dwmstatus
+which dwmstatus || (cd ~/src/dwmstatus; make; sudo make install)
+
+$PACMAN dunst sxhkd compton mate-polkit zip unzip
+$PACMAN vim
+$PACMAN pulsemixer pamixer mpc
+$PACMAN surfraw
+$PACMAN scrot
+$PACMAN emacs
+$PACMAN xorg-xrandr arandr
+
+$YAY --noconfirm starship
+test -d ~/.sdkman || curl -s "https://get.sdkman.io" | bash
