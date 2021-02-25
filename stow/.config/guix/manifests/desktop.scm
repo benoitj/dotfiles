570f857c296cb684fc79b5099c350f8263ce4028
Benoit Joly <benoit@benoitj.ca>
2021-02-24 20:47:12 -0500

    prototype of loading my own dmenu in a manifest

--- stow/.config/guix/manifests/desktop.scm
+++ stow/.config/guix/manifests/desktop.scm
@@ -1,97 +1,99 @@
-;; -*- mode: guix-scheme-*-
+;; -*- mode: guix-scheme -*-
 
 (use-modules
- (bj desktop)
- )
+ (bj desktop))
 
-       (specifications->manifest
-        '(
-          "alsa-plugins:out" ;; required by qutebrowser to play audio
-          "alsa-plugins:pulseaudio" ;; required by qutebrowser to play audio. missing LD_LIBRARY_PATH
-          "alsa-utils"
-          "pulsemixer"
-          "pamixer"
-          "gst-plugins-base"
-          "gst-plugins-good"
-          "gst-plugins-bad"
-          "gst-plugins-ugly"
-          "gstreamer"
+(concatenate-manifests
+ (list
+  (packages->manifest
+   `((,my-dmenu "out")))
+  (specifications->manifest
+   '(
+     "alsa-plugins:out" ;; required by qutebrowser to play audio
+     "alsa-plugins:pulseaudio" ;; required by qutebrowser to play audio. missing LD_LIBRARY_PATH
+     "alsa-utils"
+     "pulsemixer"
+     "pamixer"
+     "gst-plugins-base"
+     "gst-plugins-good"
+     "gst-plugins-bad"
+     "gst-plugins-ugly"
+     "gstreamer"
+     
+     ;; wm / xtools
+     "arandr"
+     "autorandr"
+     "compton"
+     "dunst"
+     "fontconfig"
+     "font-adobe-source-code-pro"
+     "font-dejavu"
+     "font-fira-code"
+     "font-google-noto"
+     "font-hack"
+     "libnotify"
+     "sxhkd"
+     "unclutter"
+     "wmctrl"
+     "xclip"
+     "xdotool"
+     "xsel"
+     "flatpak"
           
-          ;; wm / xtools
-          "arandr"
-          "autorandr"
-          "compton"
-          "dunst"
-          "fontconfig"
-          "font-adobe-source-code-pro"
-          "font-dejavu"
-          "font-fira-code"
-          "font-google-noto"
-          "font-hack"
-          "libnotify"
-          "sxhkd"
-          "unclutter"
-          "wmctrl"
-          "xclip"
-          "xdotool"
-          "xsel"
-          "flatpak"
-          
-          "scrot"
-          ;; "slip" dmenu / scrot
-          "mate-polkit"
-          "mpv"
-          "libmediainfo"
-          "feh"
-          ;; "goimapnotify"
-          ;; gopls
-          "flameshot"
-          "mupdf"
-          "poppler"
-          ;;   "libreoffice"
-          "zathura"
-          "zathura-pdf-mupdf"
-          "zathura-djvu"
-          "sxiv"
-          "gimp"
-          ;; need a replacement. brings the whole gnome desktop: "cheese"
-          
-          ;; editor
-          "emacs"
-          
-          ;; devtools
-          ;;   "clojure"
-          ;;   "clojure-lsp-bin"
-          "cmake"
-          "git"
-          "go"
-          "go-github.com-howeyc-gopass"
-          "guile"
-          ;;   "leiningen"
-          "maven"
-          "openjdk@11."
-          "ruby"
-          "tidy"
-          ;; TODO enable docker
-          ;;   "docker"
-          
-          ;; why these tools
-          ;;"perl-authen-sasl"
-          ;;"perl-net-smtp-ssl"
-          ;;"perl-mime-tools"
-          
-          ;; browsers
-          "qutebrowser"
-          "w3m"
-          "lynx"
-          "surfraw"
-          ;;   "firefox"
-          
-          ;; network tools
-          "transmission" ;; out and gui
-          "youtube-dl"
-          
-          ))
+     "scrot"
+     ;; "slip" dmenu / scrot
+     "mate-polkit"
+     "mpv"
+     "libmediainfo"
+     "feh"
+     ;; "goimapnotify"
+     ;; gopls
+     "flameshot"
+     "mupdf"
+     "poppler"
+     ;;   "libreoffice"
+     "zathura"
+     "zathura-pdf-mupdf"
+     "zathura-djvu"
+     "sxiv"
+     "gimp"
+     ;; need a replacement. brings the whole gnome desktop: "cheese"
+     
+     ;; editor
+     "emacs"
+     
+     ;; devtools
+     ;;   "clojure"
+     ;;   "clojure-lsp-bin"
+     "cmake"
+     "git"
+     "go"
+     "go-github.com-howeyc-gopass"
+     "guile"
+     ;;   "leiningen"
+     "maven"
+     "openjdk@11."
+     "ruby"
+     "tidy"
+     ;; TODO enable docker
+     ;;   "docker"
+     
+     ;; why these tools
+     ;;"perl-authen-sasl"
+     ;;"perl-net-smtp-ssl"
+     ;;"perl-mime-tools"
+     
+     ;; browsers
+     "qutebrowser"
+     "w3m"
+     "lynx"
+     "surfraw"
+     ;;   "firefox"
+     
+     ;; network tools
+     "transmission" ;; out and gui
+     "youtube-dl"
+     ))))
 
 ;; others
 ;; 
