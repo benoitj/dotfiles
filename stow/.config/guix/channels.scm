281704bc803980646ce3125c7d5543ae998d68e1
Benoit Joly <benoit@benoitj.ca>
2021-03-04 08:39:00 -0500

    enabling my-channel + emacs-native-comp channel

--- stow/.config/guix/channels.scm
+++ stow/.config/guix/channels.scm
@@ -9,4 +9,21 @@
           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
           (openpgp-fingerprint
            "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+       (channel
+        (name 'benoitj)
+        (url "https://github.com/benoitj/my-guix-channel.git")
+	(branch "main")
+        (introduction
+         (make-channel-introduction
+          "724e80321be1ef48dd56254a3ed7ef3b523bedaf"
+          (openpgp-fingerprint
+           "EFD0 C2C5 8A1B 9549 FD6F  45B5 5EBD C9DD 393D 3897"))))
+       (channel
+        (name 'flat)
+        (url "https://github.com/flatwhatson/guix-channel.git")
+        (introduction
+         (make-channel-introduction
+          "33f86a4b48205c0dc19d7c036c85393f0766f806"
+          (openpgp-fingerprint
+           "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
        %default-channels)
