e98d7bebc06f80f8ed696a5f62f70215fff782bb
Benoit Joly <benoit@benoitj.ca>
2021-03-09 21:11:34 -0500

    user based daemon using shepherd

---
+++ stow/.config/shepherd/init.scm
@@ -0,0 +1,34 @@
+;;(define gpg-agent
+;;  (make <service>
+;;    #:provides '(gpg-agent)
+;;    #:respawn? #t
+;;    #:start (make-system-constructor "gpg-connect-agent /bye")
+;;    #:stop (make-system-destructor "gpgconf --kill gpg-agent")))
+;;
+;;(define mcron
+;;  (make <service>
+;;    #:provides '(mcron)
+;;    #:respawn? #t
+;;    #:start (make-forkexec-constructor '("mcron"))
+;;    #:stop  (make-kill-destructor)))
+;;
+;;(define syncthing
+;;  (make <service>
+;;    #:provides '(syncthing)
+;;    #:respawn? #t
+;;    #:start (make-forkexec-constructor '("syncthing" "-no-browser"))
+;;    #:stop  (make-kill-destructor)))
+
+(define pulseaudio
+  (make <service>
+    #:provides '(pulseaudio)
+    #:respawn? #t
+    #:start (make-forkexec-constructor '("pulseaudio"))
+    #:stop  (make-kill-destructor)))
+
+;(register-services gpg-agent mcron syncthing pulseaudio)
+(register-services pulseaudio)
+(action 'shepherd 'daemonize)
+
+;; Start user services
+(for-each start '(pulseaudio))
