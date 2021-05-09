c95f038a2440a8a55ff4bbacc0c84d3effa66508
Benoit Joly <benoit@benoitj.ca>
2021-05-08 21:21:12 -0400

    updated command-log keymaps

--- stow/.config/doom/modules/tools/command-log/config.el
+++ stow/.config/doom/modules/tools/command-log/config.el
@@ -1,13 +1,12 @@
 ;;; tools/command-log/config.el -*- lexical-binding: t; -*-
 
 (use-package! command-log-mode
-  :commands (command-log-mode global-command-log-mode)
-  )
+  :commands (command-log-mode global-command-log-mode))
 
-;;(map! :leader
-;;      (:prefix "t"
-;;        :desc "command-log-mode" "c" #'global-command-log-mode))
-;;
-;;(after! command-log-mode
-;;  (map! :leader
-;;        :desc "command-log-buffer" "o c" #'clm/toggle-command-log-buffer))
+(map! :leader
+      (:prefix "t"
+       :desc "command-log-mode" "C" #'global-command-log-mode))
+
+(map! :leader
+      (:prefix o
+      :desc "command-log-buffer" "c" #'clm/toggle-command-log-buffer))
