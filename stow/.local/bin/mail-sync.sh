79cc459da8f056f95ecb3c9457847d7b738d9944
Benoit Joly <benoit@benoitj.ca>
2021-05-16 18:44:39 -0400

    new mail setup

--- stow/.local/bin/mail-sync.sh
+++ stow/.local/bin/mail-sync.sh
@@ -9,11 +9,11 @@ notmuch new --no-hooks 2>&1 | grep -v 'Note: Ignoring non-mail'
 # Archive files that are NOT tagged Inbox
 #filter="folder:company/INBOX -tag:inbox"
 #echo "`notmuch count $filter` archived messages"
-#notmuch search --output=files --format=text0 $filter | xargs -0 --no-run-if-empty mv -t ~/mail/company/Archive/new/
+#notmuch search --output=files --format=text0 $filter | xargs -0 --no-run-if-empty mv -t ~/Mail/company/Archive/new/
 
 #filter="tag:deleted tag:spam tag:junk AND (folder:/company/INBOX/ OR folder:/company/Archive/ AND NOT folder:/Junk/)"
 #echo "`notmuch count $filter` junk messages"
-#notmuch search --output=files --format=text0 $filter | xargs -0 --no-run-if-empty mv -t ~/mail/company/Junk\ E-Mail/new/
+#notmuch search --output=files --format=text0 $filter | xargs -0 --no-run-if-empty mv -t ~/Mail/company/Junk\ E-Mail/new/
 
 filter="tag:deleted AND folder:mailbox.org/Inbox"
 echo "`notmuch count $filter` deleted messages"
