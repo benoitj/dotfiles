a47891834b21efe06ffb0c78016e206d0aaa511e
Benoit Joly <benoit@benoitj.ca>
2021-05-30 22:32:59 -0400

    fixing emacs shemacs2 with doom

--- stow/.emacs-profiles.el
+++ stow/.emacs-profiles.el
@@ -1,3 +1,4 @@
 (
- ("default" . ((user-emacs-directory . "~/.config/doom-emacs")))
+ ("default" . ((user-emacs-directory . "~/.config/doom-emacs")
+	    (env . (("DOOMDIR" . "~/.config/doom")))))
  ("temp" . ((user-emacs-directory . "~/.emacs.temp.d"))))
