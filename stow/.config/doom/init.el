93bb8d9a450a059cef9820722b1d70865d95bb08
Benoit Joly <benoit@benoitj.ca>
2021-06-04 23:09:44 -0400

    minor emacs config changes

--- stow/.config/doom/init.el
+++ stow/.config/doom/init.el
@@ -121,8 +121,8 @@
        ;;agda              ; types of types of types of types...
        asciidoc
        ;;beancount         ; mind the GAAP
-       ;;cc                ; C > C++ == 1
-       ;;clojure           ; java with a lisp
+       (cc)                ; C > C++ == 1
+       clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
@@ -139,7 +139,7 @@
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
-       ;;(go +lsp)         ; the hipster dialect
+       (go +lsp)         ; the hipster dialect
        ;;(haskell)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
