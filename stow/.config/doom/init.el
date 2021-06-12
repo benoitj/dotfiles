7c3094b733bcff72aa38b8daab100ec7c3ce6b76
Benoit Joly <benoit@benoitj.ca>
2021-06-12 17:50:30 -0400

    fix notmuch config issue when not installed

--- stow/.config/doom/init.el
+++ stow/.config/doom/init.el
@@ -85,7 +85,8 @@
        :checkers
        syntax              ; tasing you for every semicolon you forget
        (spell
-        +aspell)   ; tasing you for misspelling mispelling
+        +aspell
+        +flyspell)   ; tasing you for misspelling mispelling
        grammar           ; tasing grammar mistake every you make
 
        :tools
