ae5d809c0533ecebf947f1ade7e01f18eb3e6d63
Benoit Joly <benoit@benoitj.ca>
2021-07-09 13:18:24 -0400

    meme module

--- dot_config/doom/modules/tools/meme/config.el
+++ dot_config/doom/modules/tools/meme/config.el
@@ -1,4 +1,9 @@
 ;;; tools/meme/config.el -*- lexical-binding: t; -*-
 
 (use-package! meme
-  :commands meme-file meme)
+  :commands (meme-file meme))
+
+(map! :leader
+      (:prefix-map ("o M" . "meme")
+       (:desc "Meme" "M" #'meme)
+       (:desc "file" "f" #'meme-file)))
