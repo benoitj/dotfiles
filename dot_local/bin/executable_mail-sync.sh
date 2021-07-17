46b7463612c88b644554f24ad3620fe1f163f5d6
Benoit Joly <benoit@benoitj.ca>
2021-07-16 21:00:22 -0400

    fix deleting emails

--- dot_local/bin/executable_mail-sync.sh
+++ dot_local/bin/executable_mail-sync.sh
@@ -15,17 +15,15 @@ notmuch new --no-hooks 2>&1 | grep -v 'Note: Ignoring non-mail'
 #echo "`notmuch count $filter` junk messages"
 #notmuch search --output=files --format=text0 $filter | xargs -0 --no-run-if-empty mv -t ~/Mail/company/Junk\ E-Mail/new/
 
-filter="tag:deleted AND folder:mailbox.org/Inbox"
-echo "`notmuch count $filter` deleted messages"
+filter="tag:deleted"
+echo "$(notmuch count $filter) deleted messages"
 notmuch search --output=files --format=text0 $filter | xargs -0 --no-run-if-empty rm -f
 
 # Make sure we have updated paths
 notmuch new --no-hooks 2>&1 | grep -v 'Note: Ignoring non-mail'
-
 
 [[ $# -eq 0 ]] && sync="-a" || sync="$1"
 
 mbsync $sync
 
 notmuch new 2>&1 | grep -v 'Note: Ignoring non-mail'
-
