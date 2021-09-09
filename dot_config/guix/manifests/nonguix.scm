7dc082081a50be960003694654302bde49468083
Benoit Joly <benoit@benoitj.ca>
2021-09-09 13:02:26 -0400

    guix and nix changes

---
+++ dot_config/guix/manifests/nonguix.scm
@@ -0,0 +1,14 @@
+;; -*- mode: guix-scheme -*-
+
+(use-modules
+ (bj desktop))
+
+(define tools-manifest
+  (specifications->manifest
+   '("emacs-native-comp"
+     "font-fira-code"
+     )))
+
+(concatenate-manifests
+ (list
+  tools-manifest))
