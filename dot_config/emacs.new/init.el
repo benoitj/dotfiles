c236416ef734ab2dd7384220d0910849cf02534c
Benoit Joly <benoit@benoitj.ca>
2021-10-04 11:44:19 -0400

    cleanup header

--- dot_config/emacs.new/init.el
+++ dot_config/emacs.new/init.el
@@ -1,30 +1,4 @@
-;; init.el --- my new emacs-lisp configuration -*- lexical-binding: t; -*-
-
-;; Copyright (C) 2021  Adam Porter
-
-;; Author:  Benoit Joly <benoit@benoitj.ca>
-;; Keywords:
-;; Package-Requires: ((emacs "28.0"))
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-;;; Commentary:
-
-;; This is the nth version of my config. This time, I'm back to elisp
-;; configuration.
-
-;;; Code:
+;;; -*- lexical-binding: t; -*-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; garbage collection tuning
