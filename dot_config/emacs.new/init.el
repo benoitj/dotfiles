5992bceaa5340c46271630702f74adce924a1792
Benoit Joly <benoit@benoitj.ca>
2021-10-10 22:05:07 -0400

    move custom config out of init.el

--- dot_config/emacs.new/init.el
+++ dot_config/emacs.new/init.el
@@ -12,6 +12,13 @@
 (add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold bj-20MB)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; move custom config to a file
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; monitor performance
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun bj-display-startup-time ()
@@ -630,17 +637,3 @@ The directory name must be absolute."
     "oMf"  '(meme-file :which-key "meme-file")))
 
 (use-package imgur)
-
-;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package "use-package")))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
