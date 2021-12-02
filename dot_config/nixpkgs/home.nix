d863005cde72028204431382780803351fffea4b
Benoit Joly <benoit@benoitj.ca>
2021-12-02 14:43:40 -0500

    imap/notmuch/goimapnotify setup

--- dot_config/nixpkgs/home.nix
+++ dot_config/nixpkgs/home.nix
@@ -15,7 +15,29 @@
     ncspot
     cups
     notmuch
+    isync
   ];
+
+  services.imapnotify.enable = true;
+
+  accounts.email.accounts.benoit_at_benoitj_ca = {
+    primary = true;
+    address = "benoit@benoitj.ca";
+    imapnotify = {
+      enable = true;
+      boxes = [ "Inbox" ];
+      onNotify = "mail-sync.sh && notify-send 'New email'";
+    };
+    imap = {
+      host = "mail.benoitj.ca";
+      port = 993;
+      tls = {
+        enable = true;
+      };
+    };
+    userName = "benoit@benoitj.ca";
+    passwordCommand = [ "passp" "mailbox.org" ];
+  };
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
